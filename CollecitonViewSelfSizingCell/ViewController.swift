8e207f8b6dae132937148aa0a741e346b752bcfa
DennisNewMachine <dnsljnv411@gmail.com>
2022-05-23 14:47:56 +0300

    Add collection to VC

--- CollecitonViewSelfSizingCell/ViewController.swift
+++ CollecitonViewSelfSizingCell/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var collection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collection = UICollectionView(frame: .null, collectionViewLayout: layout)
+        collection.backgroundColor = .orange
+        return collection
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        setUpCollectionConstraints()
     }
-
-
+    
+    private func setUpCollectionConstraints() {
+        view.addSubview(collection)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        
+        collection.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collection.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        collection.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.25).isActive = true
+        collection.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
 
